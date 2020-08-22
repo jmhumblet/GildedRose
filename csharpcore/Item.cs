f794aae7bc26aa2d1f90f7edd50ffd565e6dd87f
JiM <jmhumblet@gmail.com>
2020-08-22 07:59:04 +0200

    Remove references to Backstage pass in Item

--- csharpcore/Item.cs
+++ csharpcore/Item.cs
@@ -65,7 +65,7 @@
 
         public virtual void UpdateQuality()
         {
-            if (Name != "Aged Brie" && Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (Name != "Aged Brie")
             {
                 if (Quality > 0)
                 {
@@ -77,26 +77,6 @@
                 if (Quality < MAXIMUM_QUALITY)
                 {
                     Quality += 1;
-
-                    if (Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-
-                        if (SellIn < 6)
-                        {
-                            if (Quality < MAXIMUM_QUALITY)
-                            {
-                                Quality = Quality + 1;
-                            }
-                        }
-
-                        if (SellIn < 11)
-                        {
-                            if (Quality < MAXIMUM_QUALITY)
-                            {
-                                Quality = Quality + 1;
-                            }
-                        }
-                    }
                 }
             }
 
@@ -106,18 +86,11 @@
             {
                 if (Name != "Aged Brie")
                 {
-                    if (Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (Quality > 0)
                     {
-                        if (Quality > 0)
-                        {
                             
-                            Quality = Quality - 1;
+                        Quality = Quality - 1;
                             
-                        }
-                    }
-                    else
-                    {
-                        Quality = Quality - Quality;
                     }
                 }
                 else
